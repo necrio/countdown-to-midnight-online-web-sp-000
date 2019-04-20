266e9db71e0ea080a74090ff5e55d9d48f7f9562
Keith Isham <kisham8899@gmail.com>
2019-04-20 18:00:19 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(countdown_output)
+    countdown_output = 10
+    while countdown_output > 0
+      puts "#{countdown_output} SECOND(S)!"
+      countdown_output -= 1
+      if countdown_output == 0
+      return "HAPPY NEW YEAR!"
+    end
+        end
+
+
+end
+
+def countdown_with_sleep(sleep)
+  sleep 5
 end
